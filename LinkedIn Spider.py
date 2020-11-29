54d1732eecf986307d997b1b6de07dbb436dba7a
imbapc <45018264+imbapc@users.noreply.github.com>
2020-11-30 11:15:44 +1300

    write log

--- LinkedIn Spider.py
+++ LinkedIn Spider.py
@@ -11,6 +11,7 @@ import csv
 
 import requests
 import re
+import getpass
 
 LINKS_FINISHED = []  # 已抓取的linkedin用户
 
@@ -37,7 +38,7 @@ def login(laccount, lpassword):
     return client
 
 
-def parse(content, url):
+def parse(content, url, log_filename):
     """ 解析一个员工的Linkedin主页 """
     content = unquote(content.decode("utf-8")).replace('&quot;', '"')
     employee = {"LinkedIn-url": url}
@@ -57,9 +58,11 @@ def parse(content, url):
         else:
             employee.append(" ")
         write_csv(employee, company_name)
-        print("employee done")
+        print('.', end='')
     else:
-        print("not a valid employee")
+        print('.', end='')
+        with open(log_filename, 'a') as f:
+            f.write("not a valid employee %s\n" % url)
 
 
 def write_csv(result_employee, company_name):
@@ -77,7 +80,7 @@ def write_csv(result_employee, company_name):
             writer.writerow(result_employee)
 
 
-def crawl(url, s):
+def crawl(url, s, log_filename):
     """ 抓取每一个搜索结果 """
     try:
         if len(url) > 0 and url not in LINKS_FINISHED:
@@ -91,48 +94,61 @@ def crawl(url, s):
                     failure += 1
                     continue
                 if r.status_code == 200:
-                    parse(r.content, url)
+                    parse(r.content, url, log_filename)
                     break
                 else:
-                    print('%s %s' % (r.status_code, url))
+                    print('.', end='')
+                    with open(log_filename, 'a') as f:
+                        f.write('%s %s\n' % (r.status_code, url))
                     failure += 2
             if failure >= 10:
-                print('Failed: %s' % url)
+                print('.', end='')
+                with open(log_filename, 'a') as f:
+                    f.write('Failed: %s\n' % url)
         else:
-            print("already exists")
+            print('.', end='')
+            with open(log_filename, 'a') as f:
+                f.write("already exists : %s\n" %url)
     except Exception as e:
         pass
 
 
 if __name__ == '__main__':
     laccount = input('Input account email:')
-    lpassword = input('Input account password:')
+    lpassword = getpass.getpass('Input account password:')
     s = login(laccount=laccount, lpassword=lpassword)
-    company_name = input('Input the company you want to crawl:')
-
-    url = 'https://www.google.com/search?q=%7C+Linkedin+' + quote(
-        company_name) + '+site%3Anz.linkedin.comt&start='+str(page * 10)
+    company_name = input('Input the company name:')
+    log_filename = company_name+'log.txt'
+    print('Application is preparing data now', end='')
     results = []
     failure = 0
     for page in range(0, 3):
+        url = 'https://www.google.com/search?q=%7C+Linkedin+' + quote(
+            company_name) + '+site%3Anz.linkedin.com&start=' + str(page * 10)
         if len(url) > 0 and failure < 10:
             try:
                 r = requests.get(url, timeout=10)
             except Exception as e:
                 failure += 1
-                print('failure + 1 because of exception')
+                print('.', end='')
+                with open(log_filename, 'a') as f:
+                    f.write('failure + 1 because of exception %s\n' %url)
             if r.status_code == 200:
                 hrefs = re.findall("https://nz\..*?\&", r.text)
                 for href in hrefs:
                     href = href.replace("&", "")
                     href = href.replace("nz.linkedin.com", "www.linkedin.com")
-                    employee = crawl(href, copy.deepcopy(s))
-                    time.sleep(5)
+                    employee = crawl(href, copy.deepcopy(s), log_filename)
+                    time.sleep(1)
                 results += hrefs
                 failure = 0
             else:
                 failure += 2
-                print(r.text)
-                print('search failed: %s' % r.status_code)
+                print('.', end='')
+                with open(log_filename, 'a') as f:
+                    f.write('search failed: %s %s\n' % (r.status_code, url))
         if failure >= 10:
-            print('search failed: %s' % url)
+            print('.', end='')
+            with open(log_filename, 'a') as f:
+                f.write('search failed: %s\n' % url)
+    print("Data is written to " + company_name + ' result.csv file')
